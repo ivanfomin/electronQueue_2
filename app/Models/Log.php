2059be2b7555027887fd0a15cbb30530925dbf9b
Ivan Fomin <la-verne@mail.ru>
2018-08-01 16:40:09 +0300

    second attemp fourth homeWork

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -9,13 +9,6 @@ class Log extends Model
 
     protected $guarded = [];
 
-//    public function queuedRecords() {
-//        return Log::where('status', 1)->orderBy('created_at', 'desc')->get();
-//    }
-//
-//    public function doneRecords() {
-//        return Log::where('status', 2)->get();
-//    }
 
     public function scopeRecords($query, $status) {
         return Log::where('status', $status)->get();
